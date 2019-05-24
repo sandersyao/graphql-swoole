79b808fb314c241bdab599522ed86a1f5cec018d
pikaqiu <pikaqiu@kdd.com>
2019-05-24 23:12:05 +0800

    乱七八糟的缓冲逻辑先扔一个

---
+++ apps/Utils/Buffer.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Utils;
+
+use App\Traits\Singleton;
+
+/**
+ * 
+ */
+class Buffer
+{
+    use Singleton;
+
+    protected $map      = [];
+
+    protected $result   = [];
+
+    public function add (string $key, $value): Buffer
+    {
+        if (!isset($this->map[$key])) {
+
+            $this->map[$key]    = [];
+        }
+
+        $this->map[$key][]  = $value;
+        return $this;
+    }
+
+    public function exec (string $key, \Closure $callback)
+    {
+
+        if (!isset($this->result[$key])) {
+
+            $this->result[$key] = $callback($this->map[$key]);
+        }
+        return  $this->result[$key];
+    }
+
+    public static function clean () {
+
+        self::getInstance()->cleanAll();
+    }
+
+    public function cleanAll ()
+    {
+        $this->map  = [];
+    }
+}
